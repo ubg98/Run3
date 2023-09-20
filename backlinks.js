89bfc7cd1796521f49919bc8290f56317226b483
games235 <games235@skm.vn>
2023-09-20 16:43:42 +0000

    update

--- backlinks.js
+++ backlinks.js
@@ -1,66 +1,66 @@
 // Backlinks Code Go Here!
 const backlinksList= [{
-    link: "https://ubg89.github.io/Run2/",
-    text: "Run 2",
-  }, {
-    link: "https://html5.games235.com/g-switch-3-unblocked.html",
-    text: "GSwitch 3",
-  }, {
-    link: "https://html5.games235.com/sling-tomb-unblocked.html",
-    text: "Sling Tomb",
-  }, {
-    link: "https://html5.games235.com/jump-run-games.html",
-    text: "Jum & Run Games",
-  },];
+  link: "https://slopegame.gitlab.io/car-games.html",
+  text: "CAR",
+}, {
+  link: "https://slopegame.gitlab.io/bus-games.html",
+  text: "BUS",
+}, {
+  link: "https://slopegame.gitlab.io/truck-games.html",
+  text: "TRUCK",
+}, {
+  link: "https://slopegame.gitlab.io/bike-games.html",
+  text: "BIKE",
+},];
 
 function inFrame () {
-    try {
-        return window.self !== window.top;
-    } catch (e) {
-        return true;
-    }
-}
-
-function botBrowser() {
   try {
-    return navigator.webdriver
+      return window.self !== window.top;
   } catch (e) {
       return true;
   }
 }
 
+function botBrowser() {
+try {
+  return navigator.webdriver
+} catch (e) {
+    return true;
+}
+}
+
 function closeBacklinks() {
-  document.getElementById("backlinksPlace").style.display= "none";
-  return false;
+document.getElementById("backlinksPlace").style.display= "none";
+return false;
 }
 
 function insertBacklinks() {
-  if (inFrame()) {
-    return false;
-  }
-  let backlinksHTML= "";
-  backlinksHTML= "<ul style=\"color: #ffffff; padding: 0px; margin: 0px; font-size: 20px;\">";
-  backlinksHTML+= "<li style=\"padding: 5px; display: inline-block; float: left;\">"+
-      "<a style=\"color: #ffffff; text-decoration: none;\" href= \"//ads.games235.com\">"+ decodeURIComponent(escape("\xF0\x9F\x8F\xA0")) +"</a>"+
+if (inFrame()) {
+  return false;
+}
+let backlinksHTML= "";
+backlinksHTML= "<ul style=\"color: #ffffff; padding: 0px; margin: 0px; font-family: \"Montserrat\"; font-size: 20px;\">";
+backlinksHTML+= "<li style=\"padding: 10px; display: inline-block; float: left;\">"+
+    "<a style=\"color: #ffffff; text-decoration: none;\" href= \"//slopegame.gitlab.io\">"+ decodeURIComponent(escape("\xF0\x9F\x8F\xA0")) +"</a>"+
+    "</li>";
+for (let i=0; i< backlinksList.length; i++) {
+  backlinksHTML+= "<li style=\"padding: 10px; display: inline-block;\">"+
+      "<a style=\"color: #ffffff;\" href=\""+
+      backlinksList[i].link+ "\">"+
+      backlinksList[i].text+
+      "</a>"+
       "</li>";
-  for (let i=0; i< backlinksList.length; i++) {
-    backlinksHTML+= "<li style=\"padding: 5px; display: inline-block;\">"+
-        "<a style=\"color: #ffffff;\" href=\""+
-        backlinksList[i].link+ "\">"+
-        backlinksList[i].text+
-        "</a>"+
-        "</li>";
-  }
-  backlinksHTML+= "<li style=\"padding: 5px; display: inline-block; float: right;\">"+
-      "<a onclick= \"return closeBacklinks();\" style=\"color: #ffffff; text-decoration: none;\" href=\"#\">"+ decodeURIComponent(escape("\xE2\x9D\x8C")) +"</a>"+
-      "</li>";
-  backlinksHTML+= "</ul>";
-  const backlinksPlace = document.createElement("div");
-  backlinksPlace.setAttribute("id", "backlinksPlace");
-  backlinksPlace.style.cssText = "width: 100%; text-align: center; position: absolute; top: 0px; z-index: 999; background: #7777ff; opacity: 0.8";
-  backlinksPlace.innerHTML= backlinksHTML;
-  window.document.body.insertBefore(backlinksPlace, window.document.body.firstChild);
+}
+backlinksHTML+= "<li style=\"padding: 10px; display: inline-block; float: right;\">"+
+    "<a onclick= \"return closeBacklinks();\" style=\"color: #ffffff; text-decoration: none;\" href=\"#\">"+ decodeURIComponent(escape("\xE2\x9D\x8C")) +"</a>"+
+    "</li>";
+backlinksHTML+= "</ul>";
+const backlinksPlace = document.createElement("div");
+backlinksPlace.setAttribute("id", "backlinksPlace");
+backlinksPlace.style.cssText = "width: 100%; text-align: center; position: absolute; top: 0px; z-index: 999; background: #2C3E50; opacity: 0.8;";
+backlinksPlace.innerHTML= backlinksHTML;
+window.document.body.insertBefore(backlinksPlace, window.document.body.firstChild);
 }
 
 addEventListener("load", insertBacklinks);
-setTimeout(closeBacklinks, 60000);
+setTimeout(closeBacklinks, 5* 60* 1000);
